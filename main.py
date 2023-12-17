8e7b83098116e8536bc1da5e66a9148bd0cca6b8
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-12-17 09:39:38 +0530

    getting directories implemented

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import uvicorn
 client = MongoClient("mongodb+srv://swoyam:iiitdrive@drive.9oviyyw.mongodb.net/")
 db = client.test
 
-SECRET_KEY = "YOUR-SECRET-KEY"
+SECRET_KEY = "swoyamsiddharthnayak"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -26,6 +26,9 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -44,11 +47,12 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-@app.post("/token")
+@app.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user_dict = db["users"].find_one({"username": form_data.username})
     if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user_dict["_id"] = str(user_dict["_id"])  # Convert ObjectId to string
     user = UserInDB(**user_dict)
     password_ok = pwd_context.verify(form_data.password, user.hashed_password)
     if not password_ok:
@@ -64,8 +68,12 @@ async def signup(user: User, password: str):
     hashed_password = fake_hash_password(password)
     user_in_db = UserInDB(**user.dict(), hashed_password=hashed_password)
     db["users"].insert_one(user_in_db.dict())
+    import os
+    #this will create a folder with username in uploaded folder
+    os.makedirs(f"uploaded/{user.username}")
     return {"result": "User created"}
 
+# for testing purpose, willbe deleted later
 @app.get("/users/me")
 async def read_users_me(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -82,7 +90,26 @@ async def read_users_me(token: str = Depends(oauth2_scheme)):
     user_dict = db["users"].find_one({"username": token_data.username})
     if user_dict is None:
         raise credentials_exception
+    user_dict["_id"] = str(user_dict["_id"])  # Convert ObjectId to string
     return user_dict
+
+@app.get("/directories")
+async def get_directories(token: str = Depends(oauth2_scheme), path: str = "/"):
+    credentials_exception = HTTPException(
+        status_code=401, detail="Could not validate credentials"
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+        import os
+        entries = os.listdir(f"./uploaded/{username}"+path)
+        result_dict = {"directories": entries}
+        return result_dict
+    except JWTError:
+        raise credentials_exception
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
