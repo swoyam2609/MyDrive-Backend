39bb17f2565a5841d682199fb05b8306f60fc46a
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2023-12-18 07:59:14 +0530

    download api added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, File, UploadFile
+from fastapi.responses import FileResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import Optional
@@ -140,6 +141,24 @@ async def upload_file(token: str = Depends(oauth2_scheme), file: UploadFile=File
         }
         db["files"].insert_one(file_details)
         return {"result": "File uploaded"}
+    except JWTError:
+        raise credentials_exception
+    
+#download file
+@app.get("/download")
+async def download_file(username: str, path: str = "/"):
+    credentials_exception = HTTPException(
+        status_code=401, detail="Could not validate credentials"
+    )
+    try:
+        # payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # username: str = payload.get("sub")
+        # if username is None:
+        #     raise credentials_exception
+        # token_data = TokenData(username=username)
+        from pathlib import Path
+        file_path = f"./uploaded/{username}{path}"
+        return FileResponse(file_path, media_type='application/octet-stream', filename=file_path)
     except JWTError:
         raise credentials_exception
 
